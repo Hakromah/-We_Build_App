8c96fa14571688725e6d4511f3dfe5ec2cf9302a
Hassan <kkromah12@gmail.com>
2023-02-26 17:07:23 +0300

    new

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './NavbarStyle.scss'
+
+function Navbar() {
+  return (
+    <div className='nav-container'>
+      <h1>LOGO</h1>
+    </div>
+  )
+}
+
+export default Navbar
+
