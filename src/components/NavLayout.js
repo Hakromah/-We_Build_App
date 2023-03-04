5cc336b390cc9508bea81721ba763bbe04a9b42c
Hassan <kkromah12@gmail.com>
2023-03-05 01:32:32 +0300

    gg

---
+++ src/components/NavLayout.js
@@ -0,0 +1,24 @@
+//import About from './About';
+import Contacts from './Contacts';
+import Home from './Home';
+import Navbar from './Navbar';
+import Pages from './Pages';
+//import Services from './Services';
+import { Routes, Route } from 'react-router-dom';
+
+function NavLayout({togodark, clStyle}) {
+	return (
+		<div>
+			<Navbar togodark={togodark} clStyle={clStyle}/>
+			<Routes>
+				<Route exact path="/" element={<Home />} />
+				{/* <Route exact path="/about" element={<About />} />
+				<Route exact path="/service" element={<Services />} /> */}
+				<Route exact path="/pages" element={<Pages />} />
+				<Route exact path="/contact" element={<Contacts />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default NavLayout;
