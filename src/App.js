85d3e1cfb6a4e610de30f3314540d13e155b5afb
Hassan <kkromah12@gmail.com>
2023-03-05 01:39:09 +0300

    edited

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import About from './components/About'
+import Services from './components/Services'
 
+function App() {
+  return (
+	 <div>
+		<About/>
+		<Services/>
+	 </div>
+  )
+}
+
+export default App
