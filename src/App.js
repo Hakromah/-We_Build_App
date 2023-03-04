e9e03397301696a20c00a4c8f05102d81535bf0a
Musa Kamara <kamaramusa56@gmail.com>
2023-03-05 00:42:45 +0300

    not done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import About from "./components/About";
-import Services from "./components/Services";
+
 
 function App() {
   return (
     <div className="App">
-     <About/>
-     <Services/>
+    
     </div>
   );
 }
