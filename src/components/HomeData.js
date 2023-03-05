55260e3ac8f2148c3884ee65671f4eb2976f2039
Hassan <kkromah12@gmail.com>
2023-03-05 03:18:15 +0300

    conflicts solved

---
+++ src/components/HomeData.js
@@ -0,0 +1,22 @@
+import homeimg1 from '../assets/images/carousel-1.jpg';
+import homeimg2 from '../assets/images/carousel-2.jpg';
+
+const home_data = [
+	{
+		id: 1,
+		homIcon: <i class="fa-sharp fa-solid fa-house-chimney"></i>,
+		url: homeimg1,
+		title: 'BUILD YOUR DREAM HOUSE WITH US',
+		hbtn: 'CONTACT US',
+		rightAngle: <i class="fa-sharp fa-solid fa-angle-right"></i>,
+		leftAngle: <i class="fa-sharp fa-solid fa-angle-left"></i>
+	},
+	{
+		homIcon: <i class="fa-sharp fa-solid fa-screwdriver-wrench"></i>,
+		url: homeimg2,
+		title: 'WE ARE TRUSTED FOR YOUR PROJECT',
+		hbtn: 'GET A QUOTE',
+	},
+];
+
+export default home_data;
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,32 +1,26 @@
-import React from 'react';
-import './Service.scss';
-import db from './ServiceData';
+import React from "react";
+import "./Service.scss";
+import db from "./ServiceData";
 const Services = () => {
-	return (
-		<div className="service_container">
-			<h1 className="service_header">
-				WE PROVIDE THE BEST CONSTRUCTION SERVICES
-			</h1>
-			<div className="cards">
-				{db &&
-					db.map((items) => (
-						<div className="service_card">
-							<img
-								className="service_img"
-								src={items.img}
-								alt="portfolio"
-							/>
-							<p className="service_icon">{items.service_icon}</p>
-							<h1 className="service_title">{items.title}</h1>
-							<p className="service_text">{items.text}</p>
-							<p className="service_readMore">
-								READ MORE {items.readMore}
-							</p>
-						</div>
-					))}
-			</div>
-		</div>
-	);
+  return (
+    <div className="service_container">
+      <h1 className="service_header">
+        WE PROVIDE THE BEST CONSTRUCTION SERVICES
+      </h1>
+      <div className="cards">
+        {db &&
+          db.map((items) => (
+            <div className="service_card">
+              <img className="service_img" src={items.img} alt="portfolio" />
+              <p className="service_icon">{items.service_icon}</p>
+              <h1 className="service_title">{items.title}</h1>
+              <p className="service_text">{items.text}</p>
+              <p className="service_readMore">READ MORE {items.readMore}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Services;
