4d65ecfca960d9da1e02d2ab27cc875c2bfc13bb
Musa Kamara <kamaramusa56@gmail.com>
2023-03-13 06:59:06 +0300

    about-component-is-completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,90 +1,81 @@
-import React from 'react';
-import './About.scss';
-import signature from '../assets/images/signature.jpg';
+import React from "react";
+import "./About.scss";
+import signature from "../assets/images/signature.jpg";
 import about from '../assets/images/about.jpg';
-import box from '../assets/images/Capture.PNG';
+//import box from "../assets/images/Capture.PNG";
 const About = () => {
-	return (
-		<div className="about_container">
-		
-			<div className="about_nested about_box1">
-				<h1 className="about_title">
-					WE ARE <span className="the_lidear">THE LEADER</span> IN
-					CONSTRUCTION INDUSTRY
-				</h1>
-				<h3 className="nested-title">
-					TEMPOR ERAT ELITR AT REBUM AT AT CLITA. DIAM DOLOR DIAM IPSUM
-					TEMPOR SIT DIAM AMET DIAM ET EOS LABORE
-				</h3>
-				<p className="bottom_text">
-					Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam
-					dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam
-					diam amet diam et eos sadipscing labore. Clita erat ipsum et
-					lorem et sit, sed stet no labore lorem sit. Sanctus clita duo
-					justo et tempor
-				</p>
-				<div className="nested_box1">
-					<div>
-						<p>
-							<i class="fa-solid fa-check"></i> perfect planing
-						</p>
+  return (
+    <div className="about_container">
+      <div className="about_nested about_box1">
+        <h1 className="about_title">
+          WE ARE <span className="the_lidear">THE LEADER</span> IN CONSTRUCTION
+          INDUSTRY
+        </h1>
+        <h3 className="nested-title">
+          TEMPOR ERAT ELITR AT REBUM AT AT CLITA. DIAM DOLOR DIAM IPSUM TEMPOR
+          SIT DIAM AMET DIAM ET EOS LABORE
+        </h3>
+        <p className="bottom_text">
+          Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam dolor
+          diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam diam amet
+          diam et eos sadipscing labore. Clita erat ipsum et lorem et sit, sed
+          stet no labore lorem sit. Sanctus clita duo justo et tempor
+        </p>
+        <div className="nested_box1">
+          <div>
+            <p>
+              <i class="fa-solid fa-check"></i> perfect planing
+            </p>
 
-						<p>
-							{' '}
-							<i class="fa-solid fa-check"></i> professional Workers
-						</p>
+            <p>
+              {" "}
+              <i class="fa-solid fa-check"></i> professional Workers
+            </p>
 
-						<p>
-							{' '}
-							<i class="fa-solid fa-check"></i>first working process
-						</p>
-					</div>
-					<div>
-						<p>
-							{' '}
-							<i class="fa-solid fa-check"></i> perfect planing
-						</p>
+            <p>
+              {" "}
+              <i class="fa-solid fa-check"></i>first working process
+            </p>
+          </div>
+          <div>
+            <p>
+              {" "}
+              <i class="fa-solid fa-check"></i> perfect planing
+            </p>
 
-						<p>
-							<i class="fa-solid fa-check"></i>professional Workers
-						</p>
+            <p>
+              <i class="fa-solid fa-check"></i>professional Workers
+            </p>
 
-						<p>
-							{' '}
-							<i class="fa-solid fa-check"></i> first working process
-						</p>
-					</div>
-				</div>
-				<p className="bottom_text2">
-					Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam
-					dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam
-					diam amet diam et eos labore
-				</p>
-				<div className="about-signature">
-					<img src={signature} alt="signature" />
-				</div>
-			</div>
-			<div className="about_nested about_box2">
-				<div className="box_2_nested">
-					<img
-						className="box_2_nested_img1"
-						src={about}
-						alt="about"
-						width="50"
-						height="50"
-					/>
-				</div>
-
-				<img
-					className="box_2_nested_img2"
-					src={box}
-					alt="about"
-					width="50"
-					height="50"
-				/>
-			</div>
-		</div>
-	);
+            <p>
+              {" "}
+              <i class="fa-solid fa-check"></i> first working process
+            </p>
+          </div>
+        </div>
+        <p className="bottom_text2">
+          Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam dolor
+          diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam diam amet
+          diam et eos labore
+        </p>
+        <div className="about-signature">
+          <img src={signature} alt="signature" />
+        </div>
+      </div>
+      <div className="about_nested about_box2">
+       <div className="bg-img">
+	   <img
+          className="box_2"
+          src={about}
+          alt="about"
+          width="150"
+          height="50"
+        />
+	   </div>
+       
+      </div>
+    </div>
+  );
 };
 
 export default About;
---
+++ src/components/BlogPostData.js
@@ -0,0 +1,42 @@
+import blog1 from "../assets/images/blog-1.jpg";
+import blog2 from "../assets/images/blog-2.jpg";
+import blog3 from "../assets/images/blog-3.jpg";
+import user from "../assets/images/user.jpg";
+
+const blogData = [
+  {
+    id: 1,
+    img: blog1,
+    img2: user,
+    name: "John Doe",
+    icon: <i class="fa-regular fa-calendar-days"></i>,
+    date: "01 Jan 2045",
+    text: "REBUM DIAM CLITA LOREM ERAT MAGNA EST ERAT",
+    read: "MORE",
+    arrow: <i class="fa-sharp fa-solid fa-arrow-right"></i>,
+  },
+  {
+    id: 2,
+    img: blog2,
+    img2: user,
+    name: "John Doe",
+    icon: <i class="fa-regular fa-calendar-days"></i>,
+    date: "01 Jan 2045",
+    text: "REBUM DIAM CLITA LOREM ERAT MAGNA EST ERAT",
+    read: "MORE",
+    arrow: <i class="fa-sharp fa-solid fa-arrow-right"></i>,
+  },
+  {
+    id: 3,
+    img: blog3,
+    img2: user,
+    name: "John Doe",
+    icon: <i class="fa-regular fa-calendar-days"></i>,
+    date: "01 Jan 2045",
+    text: "REBUM DIAM CLITA LOREM ERAT MAGNA EST ERAT",
+    read: "MORE",
+    arrow: <i class="fa-sharp fa-solid fa-arrow-right"></i>,
+  },
+];
+
+export default blogData;
